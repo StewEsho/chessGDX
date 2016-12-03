87471aa6fb40b22af9355148be7d5bc5d0c7bdd1
Stew Esho <StewardEsho@gmail.com>
2016-12-02 20:29:55 -0500

    Initial Commit

---
+++ html/src/com/stewesho/chess/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.stewesho.chess.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.stewesho.chess.ChessGDX;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new ChessGDX();
+        }
+}
