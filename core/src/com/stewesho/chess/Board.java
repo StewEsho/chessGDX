259cf721627077dd169e2695d3f9797614ff59de
Stew Esho <StewardEsho@gmail.com>
2016-12-07 00:19:03 -0500

    Board now initalizes all pieces in the correct locations

--- core/src/com/stewesho/chess/Board.java
+++ core/src/com/stewesho/chess/Board.java
@@ -12,45 +12,93 @@ public class Board{
         //init fields
         gameBoard = new Piece[8][8];
 
-        for (int i = 0; i < 8; i++){
-            for (int j = 0; j < 8; j++){
+        for (int r = 0; r < 8; r++){ //rows
+            for (int c = 0; c < 8; c++){ //columns
                 //spawns pawns on first 3 rows
+                setupPiece(r, c);
 
-                switch(i){
-                default:
-                    gameBoard[i][j] = new Pawn();
-
-                case 1:
-                    gameBoard[i][j] = new Rook();
-                    break;
-
-                case 2:
-                    gameBoard[i][j] = new Knight();
-                    break;
-
-                case 3:
-                    gameBoard[i][j] = new Bishop();
-                    break;
-
-                case 4:
-                    gameBoard[i][j] = new Queen();
-                    break;
-
-                case 6:
-                    gameBoard[i][j] = new King();
-                    break;
+                try{
+                    Gdx.app.log("Board Init", gameBoard[r][c].getName());
+                } catch (NullPointerException e){
+                    Gdx.app.log("Warning", "No piece initialized at " + r + ", " + c);
                 }
-                Gdx.app.log("Board Init", gameBoard[i][j].getName());
-
             }
         }
     }
 
     public void draw(SpriteBatch _batch){
-        for (int i = 0; i < 8; i++){
-            for (int j = 0; j < 8; j++){
-                _batch.draw(gameBoard[i][j].getSprite(), 20 + (50*j), 20 + (50 * i), 32, 32);
+        for (int r = 0; r < 8; r++){
+            for (int c = 0; c < 8; c++){
+                if(gameBoard[r][c] != null) //checks that the array element exists
+                    _batch.draw(gameBoard[r][c].getSprite(), 20 + (50*c), 20 + (50 * r), 32, 32);
             }
+        }
+    }
+
+    private void setupPiece(int row, int col){
+        /**
+         * White Pieces (bottom)
+         */
+        if (row == 1){
+            this.gameBoard[row][col] = new Pawn();
+        } else if (row == 0){
+            switch(col){
+                case 0:
+                case 7: //rooks
+                    this.gameBoard[row][col] = new Rook();
+                    break;
+
+                case 1:
+                case 6: //knights
+                    this.gameBoard[row][col] = new Knight();
+                    break;
+
+                case 2:
+                case 5: //bishops
+                    this.gameBoard[row][col] = new Bishop();
+                    break;
+
+                case 3: //Queen
+                    this.gameBoard[row][col] = new Queen();
+                    break;
+
+                case 4: //king
+                    this.gameBoard[row][col] = new King();
+                    break;
+            }
+        }
+        /**
+         * Black Pieces
+         */
+        else if (row == 6){
+            this.gameBoard[row][col] = new Pawn();
+        } else if (row == 7){
+            switch(col){
+                case 0:
+                case 7: //rooks
+                    this.gameBoard[row][col] = new Rook();
+                    break;
+
+                case 1:
+                case 6: //knights
+                    this.gameBoard[row][col] = new Knight();
+                    break;
+
+                case 2:
+                case 5: //bishops
+                    this.gameBoard[row][col] = new Bishop();
+                    break;
+
+                case 3: //Queen
+                    this.gameBoard[row][col] = new Queen();
+                    break;
+
+                case 4: //king
+                    this.gameBoard[row][col] = new King();
+                    break;
+            }
+        } else {
+            this.gameBoard[row][col] = null;
         }
     }
 }
